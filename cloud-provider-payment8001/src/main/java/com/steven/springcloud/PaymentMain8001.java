04fd8d21d7cfa950e0010da9d748923a8002e249
Steven Lee <qcjy16@126.com>
2020-03-19 00:49:53 +0800

    :sparkles: Payment module initialize

---
+++ cloud-provider-payment8001/src/main/java/com/steven/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.steven.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Steven Lee
+ * Created: 2020/3/18 23:56
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    // main auto generated
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
